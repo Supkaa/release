059405d22e4b7f6327dc4cbd4f59db222fee01c1
Supkaa <mhludeev@gmail.com>
2025-03-11 23:49:37 +0500

    refactor(commitgen): remove unused funcs

--- cmd/commitgen.go
+++ cmd/commitgen.go
@@ -4,7 +4,6 @@ Copyright © 2025 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
-	"bufio"
 	"fmt"
 	"log"
 	"strings"
@@ -349,45 +348,4 @@ func (m model) viewError() string {
 	s += "\n\n" + m.errorMessage
 
 	return s
-}
-
-func selectCommitType(reader *bufio.Reader) types.CommitType {
-	fmt.Println("Select commit type:")
-	for i, t := range types.ValidCommitTypes {
-		fmt.Printf("%d: %s\n", i+1, t)
-	}
-
-	typeChoice := readChoice(reader, len(types.ValidCommitTypes))
-
-	return types.ValidCommitTypes[typeChoice-1]
-}
-
-func enterScope(reader *bufio.Reader) string {
-	fmt.Println("Enter a scope:")
-	scope, _ := reader.ReadString('\n')
-
-	return strings.TrimSpace(scope)
-}
-
-func enterShortDescription(reader *bufio.Reader) string {
-	fmt.Print("Enter a short description: ")
-	message, _ := reader.ReadString('\n')
-
-	return strings.TrimSpace(message)
-}
-
-func readChoice(reader *bufio.Reader, max int) int {
-	for {
-		fmt.Print("Enter a number: ")
-		input, _ := reader.ReadString('\n')
-		input = strings.TrimSpace(input)
-
-		for i := 1; i <= max; i++ {
-			if fmt.Sprintf("%d", i) == input {
-				return i
-			}
-		}
-
-		fmt.Println("❌ Invalid input! Please try again.")
-	}
 }
